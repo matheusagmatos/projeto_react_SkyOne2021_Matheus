461a961f25a607361915957ea9b16cb52f5423cd
matheus1999aguiar@gmail.com <matheus1999aguiar@gmail.com>
2023-07-13 11:21:26 -0300

    update

--- jogodavelha/src/index.js
+++ jogodavelha/src/index.js
@@ -14,47 +14,26 @@ function square(props){
 
 class Board extends React.Component{
 
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(nul),
-            xIsNext: true,
-        };
-    }
-
-    handleclick(i){
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState = ({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
     renderSquare(i) {
-        return <Square value={this.state.squares[i]}
-                       onClick={() => this.handleclick(i)}
+        return <Square value={this.props.squares[i]}
+                       onClick={() => this.props.onClick(i)}
                />;
     }
 
     render(){
-        const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+        
         return (
             <div>
-                <div className = "status">{status}</div>
-                /* 1 */
                 <div className = "board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
-                /* 2 */
                 <div className = "board-row">
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </div>
-                /* 3 */
                 <div className = "board-row">
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
@@ -68,15 +47,71 @@ class Board extends React.Component{
 
 class Game extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        }
+    }
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
+    handleclick(i){
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState = ({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render(){
+
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        });
         return (
             <div className = "game">
                 <div className = "game-board">
-                    <Board />
+                    <Board 
+                       squares={current.squares}
+                       onClick={(i) => this.handleclick(i)}
+                    />
                 </div>
                 <div className = "game-info">
-                    <div>{/*status*/}</div>
-                    <ol>{/*TODO*/}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         )
